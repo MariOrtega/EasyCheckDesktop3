ddf325a430b0ebf2fd22d9a7a15910acd5d4c801
Maria <remediosortegacobo@gmail.com>
2017-11-08 17:23:23 +0100

    algunos errores corregidos

--- src/clases/Reserva.java
+++ src/clases/Reserva.java
@@ -1,9 +1,11 @@
+package clases;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package easycheckserver.model;
+
 
 import com.google.gson.annotations.SerializedName;
 import java.io.Serializable;
